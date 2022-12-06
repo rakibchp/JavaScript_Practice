bc98008e0ed8e49c853d8154f9d46f0fcc40cf0b
Abdur Rakib <rakibchp@gmail.com>
2022-12-06 18:48:16 +0600

    changes are to make beauty

--- test_sample.js
+++ test_sample.js
@@ -2,7 +2,13 @@ var x = 3;
 var y = 5; 
 
 function sum(x, y) {
+    if(x>1 && y>x){
+        console.log("x is a variable");
+    }else{
+        console.log("Worst Program")
+    }
+
         return  x+y;
 }
 
-console.log(sum(x,y));
+console.log("The sum of x & y is :"+sum(x,y));
